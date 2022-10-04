ec1790df57a5ca4f108bbf2e679e99953131f90e
Patrick <106164355+Carlymax@users.noreply.github.com>
2022-10-04 19:30:38 +0300

    Error code

--- 0x04-python-more_data_structures/10-divisible_by_2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    bool_list = []
-    for number in my_list:
-        if (number % 2 == 0):
-            bool_list.append(True)
-        else:
-            bool_list.append(False)
-    return (bool_list)
